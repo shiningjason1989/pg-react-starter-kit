a339ceee882b521968c9d8cf6056b8816828d9e8
Chung Jason <shiningjason1989@gmail.com>
2016-10-06 00:22:22 +0800

    Use React Router Redux to sync route

--- src/client.js
+++ src/client.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { Router, browserHistory } from 'react-router';
+import { Router } from 'react-router';
 
 import configureStore from './utils/configureStore';
 import configureRoutes from './utils/configureRoutes';
 
+const store = configureStore();
+const routes = configureRoutes();
+
 ReactDOM.render(
-  <Provider store={configureStore()}>
-    <Router routes={configureRoutes()} history={browserHistory} />
+  <Provider store={store}>
+    <Router routes={routes} history={store.history} />
   </Provider>,
   document.getElementById('app')
 );
--- src/utils/configureStore.js
+++ src/utils/configureStore.js
@@ -1,8 +1,28 @@
-import { createStore, combineReducers } from 'redux';
+import {
+  applyMiddleware,
+  createStore,
+  combineReducers,
+  compose
+} from 'redux';
+import { browserHistory } from 'react-router';
+import {
+  routerReducer,
+  routerMiddleware,
+  syncHistoryWithStore
+} from 'react-router-redux';
 
 import * as reducers from '../reducers';
 
 export default (preloadedState) => {
-  const rootReducer = combineReducers(reducers);
-  return createStore(rootReducer, preloadedState);
+  const rootReducer = combineReducers({
+    ...reducers,
+    routing: routerReducer
+  });
+
+  const enhancer = applyMiddleware(routerMiddleware(browserHistory));
+
+  const store = createStore(rootReducer, preloadedState, enhancer);
+  store.history = syncHistoryWithStore(browserHistory, store);
+
+  return store;
 };
